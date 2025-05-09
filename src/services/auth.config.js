d55dd7c6879cce18e559424065d67f4b2f0b8c47
Mohammed Fasi <fasiulla103@gmail.com>
2025-05-09 19:45:57 +0530

    api base url updated

--- src/services/auth.config.js
+++ src/services/auth.config.js
@@ -1,3 +1,5 @@
-const API_BASE_URL = 'https://budget-tracker-react-hu18.onrender.com/api'
+const API_BASE_URL = 'https://budget-tracker-django.onrender.com/api'
+
+// const API_BASE_URL = 'http://localhost:8000/api'
 
 export default API_BASE_URL;
